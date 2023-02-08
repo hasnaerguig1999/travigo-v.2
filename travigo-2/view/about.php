8f3fcf3e23db32dfe795011d965a6d0d5cc9e1a3
hasnaerguig1999 <112939311+hasnaerguig1999@users.noreply.github.com>
2023-02-08 15:12:59 +0100

    Add files via upload

---
+++ travigo-2/view/about.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Travigo - Travel for everyone</title>
+	<link rel="stylesheet" type="text/css" href="./css/tours.css">
+
+	<link rel="stylesheet"
+  href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Paytone+One&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+</head>
+<body>
+	<!--header--->
+	<body>
+        <!--header--->
+		<header>
+			<link rel="stylessheet" type="text/css" href="./css/style.css">
+			<a href="index.php" class="logo"><img src="./img/travel_day-removebg-preview (1).png" alt="travling" 
+				width="60" 
+			 height="60"> 
+			
+				<a href="#" class="logo"></a>
+				<div class="bx bx-menu" id="menu-icon"></div>
+		
+				<ul class="navbar">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="tours.php">Tours</a></li>
+					<li><a href="about.php">About</a></li>
+					<li><a href="contactustravigo.php">Contact Us</a></li>
+				</ul>
+			</header>
+
+	<!--Home section--->
+	<section class="home" id="home">
+		<div class="home-text">
+			<h1>Travigo <br> Travel</h1>
+			<p>Explore our trips and live The Good Life with Travigo <br> Tours that make you fall in love with the world.</p>
+			<a href="#" class="home-btn">Let's go now</a>
+		</div>
+	</section>
+
+	<!--container--->
+	<section class="container">
+
+		<div class="row-items">
+			<div class="container">
+				
+				<div class="title">
+					<h2>Who are we</h2>
+				</div>
+				<br>
+				<p>Founded in 2005 by three university friends in Düsseldorf (Germany), trivago has since become a leading global accommodation search website. We are focused on reshaping the way millions of travelers search for and compare hotels and other accommodations. Part of Expedia Group (NASDAQ: TRVG), trivago’s mission is to become your companion to experience our world. </p>
+			</div>
+			<div class="container-img-b">
+				<img src="img/tourism.jpg">
+			</div>
+
+		</div>
+	</section>
+
+	<!--container--->
+	<section class="container">
+		<div class="row-items">
+			<div class="container-img-b">
+				<img src="img/maroc.jpg">
+			</div>
+			<div class="container-box">
+				<p>we organize perfect trips all over Morocco.
+					We want you to enjoy and be moved by your experience here, whether it’s your first time, or a return trip.
+					
+					We want you to see our beautiful country in the way you want to.
+					
+					We will design a trip that suits the way you travel, and what you value.
+					
+					Do you want Luxury? Adventure? Simplicity? There are so many ways to focus your time here, let’s choose the right things for you.
+					
+					Throughout, OmegaTours takes care of everything, from the day you land, until the moment we say goodbye at the airport.
+					
+					We handle all the logistics: in-country travel planning; book accommodations; and provide comfortable, private transportation.
+					
+					But the most important thing is our people: Omega’s knowledgeable, friendly guides and drivers know and love this land, and love to share it with visitors.
+					
+					We also have a wonderful network of people to host and help you go deeper in what you care to see: the freedom to explore in balance with your safety.
+					Come and see Morocco with us! You won’t be disappointed. </p>
+			</div>
+			
+
+		</div>
+	</section>
+
+	<!--destination section--->
+	<section class="destination" id="destination">
+		<div class="title">
+			<h2>Find out more</h2>
+		</div>
+
+		<div class="destination-content">
+			<div class="blog-content">
+				<img src="img/img-1.jpg">
+				<h5>Careers at trivago</h5>
+				<p>Find out more about our open roles, working culture, benefits, D&I approach and how we recruit.</p>
+			</div>
+
+			<div class="blog-content">
+				<img src="img/img-2.jpg">
+				<h5>Tech blog</h5>
+				<p>Get insight into our projects, prototypes and tech stack from our developers!</p>
+			</div>
+
+			<div class="blog-content">
+				<img src="img/img-3.jpg">
+				<h5>trivago blog</h5>
+				<p>A central source of actionable insights into hotel marketing, technology, and trends.</p>
+			</div>
+
+		</div>
+	</section>
+
+	<!--Newsletter--->
+	<section class="newsletter">
+		<div class="news-text">
+			<h2>Newsletter</h2>
+			<p>Subscribe For more HTML, CSS, and <br> coding tutorials</p>
+		</div>
+
+		<div class="send">
+			<form>
+				<input type="email" placeholder="Write Your Email" required>
+				<input type="submit" value="Submit">
+			</form>
+		</div>
+	</section>
+
+	<!--footer--->
+	<section id="contact">
+		<div class="footer">
+			<div class="main">
+				<div class="list">
+					<h4>Quick Links</h4>
+					<ul>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Terms & Conditions</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Help</a></li>
+						<li><a href="#">Tours</a></li>
+					</ul>
+				</div>
+
+				<div class="list">
+					<h4>Support</h4>
+					<ul>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Terms & Conditions</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Help</a></li>
+						<li><a href="#">Tour</a></li>
+					</ul>
+				</div>
+
+				<div class="list">
+					<h4>Contact Info</h4>
+					<ul>
+						<li><a href="#">98 West 21th Street</a></li>
+						<li><a href="#">New York NY 10016</a></li>
+						<li><a href="#">+(123)-123-1234</a></li>
+						<li><a href="#">info@travigo.com</a></li>
+						<li><a href="#">+(123)-123-1234</a></li>
+					</ul>
+				</div>
+
+				<div class="list">
+					<h4>Connect</h4>
+					<div class="social">
+						<a href="#"><i class='bx bxl-facebook' ></i></a>
+						<a href="#"><i class='bx bxl-instagram-alt' ></i></a>
+						<a href="#"><i class='bx bxl-twitter' ></i></a>
+						<a href="#"><i class='bx bxl-linkedin' ></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="end-text">
+			<p>Copyright ©2022 All rights reserved | Travigo</p>
+		</div>
+	</section>
+
+	<!--link to js--->
+	<script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
